15d94974cf71a23b9bc03c4f3ea6f38fa1bfa759
Anna Panferova <anyapanf@gmail.com>
2024-07-23 18:51:30 +0600

    Island, Sky, Bird and Plane components are added

---
+++ src/models/plane.jsx
@@ -0,0 +1,23 @@
+import { useAnimations, useGLTF } from '@react-three/drei';
+import birdScene from '../../assets/3d/plane.glb';
+import { useEffect, useRef } from 'react';
+
+export const Plane = ({ isRotating, ...props }) => {
+    const ref = useRef();
+    const { scene, animations } = useGLTF(birdScene);
+    const { actions } = useAnimations(animations, ref);
+
+    useEffect(() => {
+        if (isRotating) {
+            actions['Take 001'].play();
+        } else {
+            actions['Take 001'].stop();
+        }
+    }, [actions, isRotating]);
+
+    return (
+        <mesh {...props} ref={ref}>
+            <primitive object={scene} />
+        </mesh>
+    )
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,73 @@
-export const Home = () => {
-  return (
-    <section className="w-full h-screen relative">
-      {/* <div className="absolute top-28 left-0 right-0 z=-10 flex items-center justify-center">
+import { Canvas } from "@react-three/fiber";
+import { useState, Suspense } from "react";
+import { Loader } from "../components/loader";
+import { Island } from "../models/island";
+import { Sky } from "../models/sky";
+import { Bird } from "../models/bird";
+import { Plane } from "../models/plane";
+
+{/* <div className="absolute top-28 left-0 right-0 z=-10 flex items-center justify-center">
         POPUP
       </div> */}
+
+export const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
+  const ajustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    };
+
+    return [screenScale, screenPosition, rotation]
+  };
+
+  const ajustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    };
+
+    return [screenScale, screenPosition]
+  };
+
+  const [islandScale, islandPosition, islandRotation] = ajustIslandForScreenSize();
+  const [planeScale, planePosition] = ajustPlaneForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+        camera={{ near: 0.1, far: 1000 }}>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
+
+          <Bird />
+          <Sky isRotating={isRotating}/>
+          <Island position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage} />
+          <Plane position={planePosition}
+            scale={planeScale}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]} />
+        </Suspense>
+      </Canvas>
     </section>
   )
 };
